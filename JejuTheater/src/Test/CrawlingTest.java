960621478e37e1737aa210d71dfb912ab0e1a75a
voidblue <kimqwe12300@daum.net>
2018-01-08 14:14:36 +0900

    로컬 라이브러리 파일 추가

--- JejuTheater/src/Test/CrawlingTest.java
+++ JejuTheater/src/Test/CrawlingTest.java
@@ -2,6 +2,7 @@ package Test;
 
 import Utils.Crawling;
 import org.jsoup.nodes.Document;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,6 +20,7 @@ public class CrawlingTest {
     @Test
     public void parse() throws IOException {
         httpdocument = crawling.parse("http://www.cgv.co.kr/theaters/?areacode=206%2C04%2C06&theaterCode=0121&date=20180103");
+        Assert.assertTrue(httpdocument.title().equals("영화 그 이상의 감동. CGV"));
     }
 
     @Test
