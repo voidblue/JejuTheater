a1de20f60d655fbc3f00e16f14516107d5619e9b
ssoso27 <sohi8290@jejunu.ac.kr>
2018-01-08 16:00:02 +0900

    [https://trello.com/c/UySNqHco] 크롤링 파라미터 변경

--- JejuTheater/src/Crawlers/JunitCralwer.java
+++ JejuTheater/src/Crawlers/JunitCralwer.java
@@ -10,15 +10,13 @@ import java.util.Date;
 
 public class JunitCralwer implements Crawl{
     @Override
-    public String crawl(String url, String dateformat) {
-        System.out.println(" Start Date : " + dateformat);
+    public String crawl(String url) {
         Document httpdocument = null;
         try {
-            httpdocument = (Document) Jsoup.connect(url + dateformat).get();
+            httpdocument = (Document) Jsoup.connect(url).get();
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-        System.out.println(" End Date : " + dateformat);
         return httpdocument.outerHtml();
     }
 }
--- JejuTheater/src/Test/CrawlerTest.java
+++ JejuTheater/src/Test/CrawlerTest.java
@@ -8,6 +8,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.swing.text.html.HTMLDocument;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CrawlerTest {
     public static Crawler crawler;
@@ -18,10 +21,22 @@ public class CrawlerTest {
 
     @Test
     public void CGVtest(){
-        String httpdocument = crawler.crawl(new JunitCralwer(),"http://www.cgv.co.kr/theaters/?areacode=206%2C04%2C06&theaterCode=0121","&date=20180108");
+        String date = getDate();
+        String cgv_url = "http://www.cgv.co.kr/theaters/?areacode=206%2C04%2C06&theaterCode=0121" + "&date=" + date;
+
+        String httpdocument = crawler.crawl(new JunitCralwer(),cgv_url);
+        System.out.println(httpdocument);
         Assert.assertTrue(httpdocument.contains("CGV"));
     }
 
+    private String getDate()
+    {
+        Calendar c1 = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); // 날짜 포맷
+        String date = sdf.format(c1.getTime()); // String으로 저장
+
+        return date;
+    }
 
 
 }
