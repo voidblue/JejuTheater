dbbee92574e3fd93250f5b010e9e74ca2aa641f4
ssoso27 <sohi8290@jejunu.ac.kr>
2018-01-16 11:41:19 +0900

    파싱 결과 Data 담을 ArrayList 생성

---
+++ JejuTheater/src/TheaterData/Movies.java
@@ -0,0 +1,13 @@
+package TheaterData;
+
+import java.util.ArrayList;
+
+public class Movies extends ArrayList {
+    String title;
+    String title_en;
+    String genre;
+    String storyline;
+    String release_date;
+    String age_limit;
+    String ticket_sales;
+}
---
+++ JejuTheater/src/TheaterData/Schedules.java
@@ -0,0 +1,13 @@
+package TheaterData;
+
+import java.util.ArrayList;
+
+public class Schedules extends ArrayList {
+    String month;
+    String day;
+    String day_of_week;
+    String movie_title;
+    String screen_number;
+    String show_time;
+    String seat_left;
+}
