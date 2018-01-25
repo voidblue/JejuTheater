d2886fd6899d35e06fd62b0e9da5c973cf09fc25
LEGOLLAS <gurwl1885@gmail.com>
2018-01-25 15:37:07 +0900

    JSON 형태 완료

--- JejuTheater/src/Crawlers/JsoupCralwer.java
+++ JejuTheater/src/Crawlers/JsoupCralwer.java
@@ -11,14 +11,14 @@ import java.io.IOException;
 
 //Selenium보다 속도가 훨~~~씬 빠르니까 이걸로 써요
 //동적 웹에는 사용하기 힘듦
-public class JsoupCralwer implements Crawl{
+public class JsoupCralwer implements Crawl {
     @Override
     public String crawl(String url) {
         Document httpdocument = null;
         try {
             httpdocument = (Document) Jsoup.connect(url).get();
 //            httpdocument = (Document) Jsoup.connect(url).m
-        }catch (IOException e){
+        } catch (IOException e) {
 //            System.out.println(e.getMessage());
         }
 
--- JejuTheater/src/Utils/Json.java
+++ JejuTheater/src/Utils/Json.java
@@ -7,42 +7,82 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Json {
-    JSONArray jsonArray;
+    JSONObject schedule;
     JSONObject externalObject;
-    JSONObject internalObject;
     ArrayList<JSONObject> list;
 
 
     public Json(){
-        jsonArray = new JSONArray();
+        schedule = new JSONObject();
         externalObject = new JSONObject();
+
         list = new ArrayList<>();
         try {
-            externalObject.put("영화", jsonArray);
-            internalObject = new JSONObject();
-            internalObject.put("name", "matrix");
-            list.add(internalObject);
-            internalObject = new JSONObject();
-            internalObject.put("name", "matrix2");
-            list.add(internalObject);
-            internalObject = new JSONObject();
-            internalObject.put("name", "matrix3");
-            list.add(internalObject);
-            internalObject = new JSONObject();
-            internalObject.put("name", "matrix4");
-            list.add(internalObject);
+            externalObject = new JSONObject();
+            externalObject.put("schedule", schedule);
+            schedule.put("date", "date");
+            JSONObject theater = new JSONObject();
+            schedule.put("theater", theater);
+
+            theater.put("brand", "brand");
+            theater.put("theaterName", "theaterName");
+            theater.put("address", "address");
+            theater.put("phoneNumber", "phoneNumber");
+            JSONArray movies = new JSONArray();
+            theater.put("movies", movies);
+            JSONObject movie = new JSONObject();
+            movies.put(movies.length(), movie);
+
+            movie.put("movieId", "movieId");
+            movie.put("movieName", "movieName");
+            movie.put("movieNameENG", "movieNameENG");
+            movie.put("genre", "genre");
+            movie.put("ageLimit", "age");
+            movie.put("openDate", "date");
+            movie.put("story", "story");
+            movie.put("share", "share");
+            movie.put("sales", "sales");
+            JSONArray timeList = new JSONArray();
+            movie.put("timeList", timeList);
+
+            JSONObject showTime = new JSONObject();
+            timeList.put(showTime);
+            showTime.put("screenId", "screenId");
+            showTime.put("room", "room");
+            showTime.put("time", "time");
+            showTime.put("leftSeat", "leftSeat");
+            showTime.put("totalSeat", "totalSeat");
+//            internalObject.put("Date", "today");
+//            list.add(internalObject);
+//
+//
+//
+//            externalObject2 = new JSONObject(schedule);
+//            externalObject2.put("Theater" , schedule);
+//
+//            getInternalObject = new JSONObject();
+//            getInternalObject.put("Brand", "");
+//            getInternalObject.put("Name", "");
+//            getInternalObject.put("Address", "");
+//            getInternalObject.put("Phone", "");
+//            list.add(getInternalObject);
+//
+//
+//            getGetInternalObject = new JSONObject();
+//            getGetInternalObject.put("name", "matrix3");
+//            list.add(getGetInternalObject);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         for (JSONObject e : list){
-            jsonArray.put(e);
+//            schedule.put(e);
         }
 
-        System.out.println(externalObject.toString());
+        System.out.println(externalObject.toString().replace("}", "}\n"));
     }
 
     public static void main(String args[]){
