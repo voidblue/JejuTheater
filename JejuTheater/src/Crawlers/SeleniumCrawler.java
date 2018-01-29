fdae3b79d35f1a121c50e7ac38b2b6ee708fe3af
ssoso27 <sohi8290@jejunu.ac.kr>
2018-01-29 17:24:46 +0900

    [lib 버전 변경] selenium -> 3.6.0 / geckodriver -> 0.19.0

--- JejuTheater/src/Crawlers/SeleniumCrawler.java
+++ JejuTheater/src/Crawlers/SeleniumCrawler.java
@@ -1,20 +1,10 @@
 package Crawlers;
 
 import Interface.Crawl;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.io.File;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 //무적 크롤러.. 동적웹에 클릭도 할수있음 다만 조금 느림
 public class SeleniumCrawler implements Crawl {
@@ -25,7 +15,7 @@ public class SeleniumCrawler implements Crawl {
         this.pager = pager;
         FirefoxBinary firefoxBinary = new FirefoxBinary();
         String path = System.getProperty("user.dir");
-        System.setProperty("webdriver.gecko.driver", path + "/driver/geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", path + "/driver/geckodriver");
 
         System.out.println(path);
 
